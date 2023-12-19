2185871baae97ab5b115eb8c68fc2efe6e2af45d
uwebury <142432719+uwebury@users.noreply.github.com>
2023-12-19 16:37:38 +0100

    Created frontend

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,20 @@
-export default function HomePage() {
-  return <h1>Hello from Next.js!</h1>;
+import useSWR from "swr";
+
+const fetcher = (...args) => fetch(...args).then((res) => res.json());
+
+export default function Character() {
+  const { data, error } = useSWR(`/api/random-character`, fetcher);
+
+  if (error) return <div>Failed to load</div>;
+  if (!data) return <div>Loading...</div>;
+
+  return (
+    <>
+      <h1>
+        {data.firstName} {data.lastName}
+      </h1>
+      <p>Twitter: {data.twitterName}</p>
+      <p>geohash: {data.geohash}</p>
+    </>
+  );
 }
